376d9fbb452648d06b82103857bccc818f14fb36
ISAQUE MARQUES DOS SANTOS <035354135@ssp.ba.intranet>
2024-10-03 15:57:14 -0300

    atualização_comentario

--- src/Forum.js
+++ src/Forum.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { collection, onSnapshot, query, orderBy, doc, updateDoc, getDoc } from 'firebase/firestore';
+import { collection, onSnapshot, query, orderBy, doc, updateDoc, getDoc,addDoc} from 'firebase/firestore';
 import { db, auth } from './firebase';
 import styled from 'styled-components';
 import { FaThumbsUp } from 'react-icons/fa'; // Ícone de curtir
@@ -8,6 +8,8 @@ import { AddCircle } from '@mui/icons-material'; // Ícone de adicionar
 import { formatDistanceToNow } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR';
 import { useNavigate } from 'react-router-dom'; // Corrigido para usar react-router-dom
+
+
 
 const Container = styled.div`
   flex: 1;
@@ -24,7 +26,7 @@ const Header = styled.header`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  width: 88%;
+  width: 80%;
   border-radius: 10px;
   margin-left: 280px; /* Define a margem esquerda */
 `;
@@ -70,7 +72,7 @@ const ForumItem = styled.div`
 `;
 
 const ForumImage = styled.img`
-  width: auto;
+  width: 80%;
   height: 10%;
   background-color: #e9ecef; /* Cinza claro para imagens sem fundo */
   loading: lazy;
@@ -130,7 +132,7 @@ const ReactionContainer = styled.div`
 `;
 
 const ReactionButton = styled.button`
-  padding: 8px;
+  padding: 0px;
   background: none;
   border: none;
   cursor: pointer;
@@ -141,7 +143,8 @@ const ReactionCountText = styled.span`
   color: #000; /* Preto para contagem de reações */
   font-size: 20px;
   font-weight: bold;
-   padding: 8px;
+   padding: 0px;
+    margin-left: -80px;
 
 `;
 
@@ -186,10 +189,78 @@ const SidebarItem = styled.div`
     background-color: #1ABC9C;  // Cor suave ao passar o mouse
   }
 `;
+const CommentSection = styled.div`
+  width: 90%;
+  margin-top: 10px;
+  background-color: #f9f9f9;
+  padding: 16px;
+  border-radius: 8px;
+`;
+
+const CommentContainer = styled.div`
+  display: flex;
+  margin-bottom: 10px;
+  align-items: flex-start;
+`;
+
+const CommentUserPhoto = styled.img`
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  margin-right: 12px;
+`;
+
+const CommentContent = styled.div`
+  
+`;
+
+const CommentUserName = styled.span`
+  font-weight: bold;
+  color: #333;
+  font-size: 14px;
+`;
+
+const CommentText = styled.p`
+  color: #555;
+  margin: 4px 0;
+  font-size: 14px;
+`;
+
+const CommentDate = styled.span`
+  color: #999;
+  font-size: 12px;
+`;
+
+const CommentActions = styled.div`
+  display: flex;
+  align-items: center;
+  margin-top: 8px;
+  gap: 16px;
+`;
+
+const ActionButton = styled.button`
+  background: none;
+  border: none;
+  display: flex;
+  align-items: center;
+  color: #666;
+  cursor: pointer;
+
+  &:hover {
+    color: #000;
+  }
+`;
+
+const CommentLikeIcon = styled(FaThumbsUp)`
+  margin-right: 6px;
+`;
+
+const CommentReplyIcon = styled(BiChat)`
+  margin-right: 6px;
+`;
 
 
 
-// Estilos aqui ...
 
 export default function Forum() {
   const [forums, setForums] = useState([]);
@@ -197,6 +268,8 @@ export default function Forum() {
   const [loading, setLoading] = useState(true);
   const [visibleContent, setVisibleContent] = useState({});
   const [searchTerm, setSearchTerm] = useState('');
+  const [comments, setComments] = useState({});
+  const [newComment, setNewComment] = useState({}); // Para novos comentários
 
   const userId = auth.currentUser?.uid;
   const navigate = useNavigate(); // Use useNavigate para navegação na web
@@ -234,21 +307,21 @@ export default function Forum() {
   const handleLike = async (postId) => {
     const postRef = doc(db, 'forums', postId);
     const postDoc = await getDoc(postRef);
-  
+
     if (postDoc.exists()) {
       const postReactions = postDoc.data().reactions || {};
       const userReaction = postReactions[userId];
-  
+
       const updatedReactions = { ...postReactions };
-  
+
       if (userReaction === 'like') {
         delete updatedReactions[userId]; // Remove like se já foi curtido
       } else {
         updatedReactions[userId] = 'like'; // Adiciona ou atualiza a reação
       }
-  
+
       await updateDoc(postRef, { reactions: updatedReactions });
-  
+
       setForums((prevForums) =>
         prevForums.map((forum) =>
           forum.id === postId ? { ...forum, reactions: updatedReactions } : forum
@@ -256,24 +329,54 @@ export default function Forum() {
       );
     }
   };
-  
-  // Condicional para alterar o estilo
+
   const hasLiked = (postId) => {
     const postReactions = forums.find(forum => forum.id === postId)?.reactions || {};
     return !!postReactions[userId]; // Verifica se o usuário já curtiu
   };
-  
 
   const getLikeCount = (postId) => {
     const postReactions = forums.find(forum => forum.id === postId)?.reactions || {};
     return Object.keys(postReactions).length;
   };
 
-  const toggleContentVisibility = (postId) => {
-    setVisibleContent(prev => ({
-      ...prev,
-      [postId]: !prev[postId],
-    }));
+  // Função para buscar comentários
+  const fetchComments = (postId) => {
+    const commentsRef = collection(db, 'forums', postId, 'comments');
+    const commentsQuery = query(commentsRef, orderBy('createdAt', 'desc'));
+
+    onSnapshot(commentsQuery, (snapshot) => {
+      const commentsData = snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+      setComments((prev) => ({
+        ...prev,
+        [postId]: commentsData
+      }));
+    });
+  };
+
+// Alterna a visibilidade dos comentários e busca comentários ao abrir
+const toggleCommentsVisibility = (postId) => {
+  setVisibleContent(prev => ({
+    ...prev,
+    [postId]: !prev[postId] // Abre ou fecha somente o post clicado
+  }));
+  
+  if (!visibleContent[postId]) {
+    fetchComments(postId); // Busca os comentários apenas se o post está sendo aberto
+  }
+};
+
+
+  const handleAddComment = async (postId) => {
+    if (newComment[postId]?.trim()) {
+      const commentsRef = collection(db, 'forums', postId, 'comments');
+      await addDoc(commentsRef, {
+        userId: userId,
+        content: newComment[postId],
+        createdAt: new Date(),
+      });
+      setNewComment((prev) => ({ ...prev, [postId]: '' })); // Limpa o campo de comentário
+    }
   };
 
   const filteredForums = forums.filter(forum => 
@@ -335,30 +438,89 @@ export default function Forum() {
             <ForumItem key={item.id}>
               {item.imageUrl && <ForumImage src={item.imageUrl} alt={item.title} />}
               <ForumTextContainer>
-                <ForumTitle onClick={() => toggleContentVisibility(item.id)}>{item.title}</ForumTitle>
+                <ForumTitle onClick={() => toggleCommentsVisibility(item.id)}>{item.title}</ForumTitle>
                 {visibleContent[item.id] && <ForumContent>{item.content}</ForumContent>}
                 <UserInfoContainer>
                   {user.photoURL ? (
                     <UserPhoto src={user.photoURL} alt={user.fullName} />
                   ) : null}
-                  <UserName>{user.fullName || 'Usuário Anônimo'}</UserName>
+                  <UserName>{user.fullName || user.firstName}</UserName>
                 </UserInfoContainer>
                 <PostDate>{postDate}</PostDate>
                 <ReactionContainer>
-                <ReactionButton onClick={() => handleLike(item.id)} style={{ color: hasLiked(item.id) ? 'black' : 'gray' }}>
-  <FaThumbsUp />
-  <ReactionCountText>{getLikeCount(item.id)}</ReactionCountText>
+               
+<ReactionButton onClick={() => handleLike(item.id)}>
+  <FaThumbsUp color={hasLiked(item.id) ? 'blue' : 'gray'} />
+</ReactionButton>
+<ReactionCountText>{getLikeCount(item.id)}</ReactionCountText>
+<ReactionButton onClick={() => toggleCommentsVisibility(item.id)}>
+  <BiChat />
 </ReactionButton>
 
-                  <ReactionButton>
-                    <BiChat />
-                  </ReactionButton>
-                </ReactionContainer>
-              </ForumTextContainer>
-            </ForumItem>
-          );
-        })}
-      </ForumList>
-    </Container>
+</ReactionContainer>
+</ForumTextContainer>
+
+{visibleContent[item.id] && (
+<div>
+{/* Exibe os comentários */}
+{comments[item.id]?.map(comment => {
+  const commentUser = users[comment.userId] || {};
+  const commentDate = formatDistanceToNow(new Date(comment.createdAt.seconds * 1000), {
+    addSuffix: true,
+    locale: ptBR,
+  });
+
+  return (
+    <div key={comment.id} style={{ marginTop: '10px' }}>
+      
+    </div>
   );
+})}
+{visibleContent[item.id] && (
+  <div>
+    {/* Exibe os comentários */}
+    {comments[item.id]?.map(comment => {
+      const commentUser = users[comment.userId] || {};
+      const commentDate = formatDistanceToNow(new Date(comment.createdAt.seconds * 1000), {
+        addSuffix: true,
+        locale: ptBR,
+      });
+
+      return (
+        <div key={comment.id} style={{ marginTop: '10px' }}>
+                  <CommentContainer>
+            {commentUser.photoURL ? (
+              <CommentUserPhoto src={commentUser.photoURL} alt={commentUser.fullName} />
+            ) : null}
+            <CommentContent>
+              <CommentUserName>{commentUser.fullName || commentUser.firstName}</CommentUserName>
+              <CommentText>{comment.text}</CommentText>
+              <CommentDate>{commentDate}</CommentDate>
+            </CommentContent>
+          </CommentContainer>
+        </div>
+      );
+    })}
+
+    {/* Adicionar novo comentário */}
+    <CommentSection>
+      <input
+        type="text"
+        placeholder="Adicionar comentário"
+        value={newComment[item.id] || ''}
+        onChange={(e) => setNewComment(prev => ({ ...prev, [item.id]: e.target.value }))}
+      />
+      <button onClick={() => handleAddComment(item.id)}>Enviar</button>
+    </CommentSection>
+  </div>
+)}
+
+</div>
+)}
+</ForumItem>
+);
+})}
+</ForumList>
+</Container>
+);
 }
